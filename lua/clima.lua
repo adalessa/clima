e788a839395dad1cde1956d3954614878600cd23
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-12-10 21:23:18 +0100

    fix

--- lua/clima.lua
+++ lua/clima.lua
@@ -1,7 +1,7 @@
 return {
   get = function()
-    vim.net.request("wttr.in/Germany?format=3", {}, vim.schedule_wrap(function(err, body)
-      print(body)
+    vim.net.request("wttr.in/Germany?format=3", {}, vim.schedule_wrap(function(err, resp)
+      print(resp.body)
     end))
   end
 }
