198266dda5526292d6f603ba530df5f3b33e9bbf
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-12-10 22:05:32 +0100

    den like

--- lua/clima.lua
+++ lua/clima.lua
@@ -3,5 +3,8 @@ return {
     vim.net.request("wttr.in/Germany?format=3", {}, vim.schedule_wrap(function(err, resp)
       print(resp.body)
     end))
+  end,
+  about = function()
+   print("dejen su like")
   end
 }
